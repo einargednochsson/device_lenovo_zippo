1debd474052622d3a8121131011e0f7df6079033
optionaltoast <robin.b.martin@gmail.com>
2021-08-20 16:11:36 -0600

    zippo: decommonize sensors
    Change-Id: Iad917c6d9026ded7ba5c28e7e4f9fff604232c13

--- device.mk
+++ device.mk
@@ -13,6 +13,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
 
