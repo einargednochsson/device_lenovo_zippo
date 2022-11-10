51df38e3202970e60a92572eb2b9d4a6c0857710
chematelegram <chfondia@gmail.com>
2022-11-10 22:33:28 +0000

    zippo: Build Aperture

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Aperture
+
 # FM
 PRODUCT_PACKAGES += \
     libqcomfm_jni \
