f014cdeb0bdade79f00299a268a62e4677b503c3
Marc Bourgoin <themard85@gmail.com>
2024-07-08 20:17:22 -0600

    zippo: Commonize audio HAL version
    Change-Id: I2c18424345699331b000b51c25ca2eecb5bf82db

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2019-2021 The LineageOS Project
+# Copyright (C) 2019-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,10 +15,6 @@ PRODUCT_PACKAGES += \
     SystemUIResZippo
 
 # Audio
-PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
