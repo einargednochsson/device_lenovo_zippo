7b2cf95b3f2ed7f71df3a07986fd523541683167
Einar Gednochsson <pete80pro.pb@gmail.com>
2025-03-31 20:50:27 +0200

    sm8150-common: axion bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_zippo.mk
+    $(LOCAL_DIR)/lineage_zippo.mk
--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -7,24 +7,22 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ICONS := true
+TARGET_DISABLE_EPPE := true
 
-# Enable quick tap
-TARGET_SUPPORTS_QUICK_TAP := true
-
+# Misc
+ AXION_CAMERA_REAR_INFO := 48,8,16,2
+ AXION_CAMERA_FRONT_INFO := 32
+ AXION_PROCESSOR := Snapdragon855
+ AXION_MAINTAINER := EinarG
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78051
-PRODUCT_NAME := derp_zippo
+PRODUCT_NAME := lineage_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
