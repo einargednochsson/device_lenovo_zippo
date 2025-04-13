6c6d1e175647db741d1b4e5006dd4f1361f147be
Einar Gednochsson <pete80pro.pb@gmail.com>
2025-04-13 12:38:02 +0200

    zippo: Axion Bringup

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -8,6 +8,13 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
+
+# Misc
+AXION_CAMERA_REAR_INFO := 48,8,16,2
+AXION_CAMERA_FRONT_INFO := 32
+AXION_PROCESSOR := Snapdragon855
+AXION_MAINTAINER := EinarG
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
