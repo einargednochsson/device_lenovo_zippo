3489ef7f30ec243c6781178e30c97d8a1514525c
Einar Gednochsson <pete80pro.pb@gmail.com>
2025-10-01 20:11:47 +0000

    zippo: include los prebuilds in gms builds

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -11,11 +11,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_DISABLE_EPPE := true
 BYPASS_CHARGE_SUPPORTED := true
 
-# Misc
-AXION_CAMERA_REAR_INFO := 48,8,16,2
-AXION_CAMERA_FRONT_INFO := 32
-PRODUCT_NO_CAMERA := false
-AXION_PROCESSOR := Snapdragon855
+# Lineage prebuilts
+ifneq ($(WITH_GMS),true) 
+TARGET_INCLUDES_LOS_PREBUILTS := true
+endif
+
+# Misc 
+AXION_CAMERA_REAR_INFO := 48,8,16,2 
+AXION_CAMERA_FRONT_INFO := 32 
+PRODUCT_NO_CAMERA := false 
+AXION_PROCESSOR := Snapdragon855 
 AXION_MAINTAINER := EinarG
 
 # Define small and big core groups (used for setting processes affinity)
