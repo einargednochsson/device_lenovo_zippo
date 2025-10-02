3bd04beeae8871a61b636be936bc863d4a113d9d
Einar Gednochsson <pete80pro.pb@gmail.com>
2025-10-02 06:10:11 +0000

    zippo: add useful apps for all versions

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -11,10 +11,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_DISABLE_EPPE := true
 BYPASS_CHARGE_SUPPORTED := true
 
-# Lineage prebuilts
-ifneq ($(WITH_GMS),true) 
+# Lineage prebuits
 TARGET_INCLUDES_LOS_PREBUILTS := true
-endif
 
 # Misc 
 AXION_CAMERA_REAR_INFO := 48,8,16,2 
